04414cdedee65f18cb74dfbf72c054d16472df87
Iso4datel <ZYXWorking@mail.ru>
2018-03-20 12:39:52 -0700

    added documentation

--- include/print.hpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <fstream>
-#include <iostream>
-
-// Функция вывода строки в поток
-void print(const std::string& text, std::ostream& out = std::cout);
-
-// Функция вывода строки в файл
-void print(const std::string& text, std::ofstream& out);
